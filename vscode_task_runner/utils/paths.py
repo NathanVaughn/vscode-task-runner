33a7ad69ab6170a61bf519c631b442c40855032f
Nathan Vaughn <nath@nvaughn.email>
2026-09-22 21:16:33 +0000

    Make local path search more robust

--- vscode_task_runner/utils/paths.py
+++ vscode_task_runner/utils/paths.py
@@ -14,15 +14,17 @@ def which_resolver(exec: str, cwd: Path) -> str:
 
     # Stupid hack to handle this, because `os.path.join` doesn't
     # handle this in the way we want.
-    exec = exec.removeprefix("./")
+    exec_stripped = exec.removeprefix("./")
+    local_only = exec_stripped != exec
 
     # https://docs.python.org/3/library/shutil.html#shutil.which
 
     # First, check the current directory.
     # If that fails, let shutil.which use the system PATH.
-    if which_result := shutil.which(exec, path=cwd):
+    if which_result := shutil.which(exec_stripped, path=cwd):
         return which_result
-    elif which_result := shutil.which(exec):
+    # Don't allow global search if the "./" prefix was applied
+    elif (not local_only) and (which_result := shutil.which(exec_stripped)):
         return which_result
     else:
         raise ExecutableNotFound(f"Executable {exec} not found")
